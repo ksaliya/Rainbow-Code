ec75237056cdb39f9c36d50c12c7e2980d73525c
ksaliya <saliyakumara@yahoo.com>
2017-03-23 15:57:18 -0400

    Update snake.js
    Added gameState variable to keep track of the state of the game

--- challenges/CC_03_Snake_game_p5.js/snake.js
+++ challenges/CC_03_Snake_game_p5.js/snake.js
@@ -1,64 +1,65 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/AaGK-fj-BAM
+function Snake(){
+	this.x = 0;
+	this.y = 0;
+	this.xspeed = 1;
+	this.yspeed = 0;
+	this.total = 1;
+	this.tail = [];
 
-function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 1;
-  this.yspeed = 0;
-  this.total = 0;
-  this.tail = [];
+	this.update = function(){
+		for(var i = 0; i < this.tail.length - 1; i++){
+			this.tail[i] = this.tail[i+1];
+		}
+		this.tail[this.total-1] = createVector(this.x, this.y);
 
-  this.eat = function(pos) {
-    var d = dist(this.x, this.y, pos.x, pos.y);
-    if (d < 1) {
-      this.total++;
-      return true;
-    } else {
-      return false;
-    }
-  }
+		this.x += this.xspeed*gridScale;
+		this.y += this.yspeed*gridScale;
 
-  this.dir = function(x, y) {
-    this.xspeed = x;
-    this.yspeed = y;
-  }
+		this.x = constrain(this.x, 0, width - gridScale);
+		this.y = constrain(this.y, 0, height - gridScale);
+	}
 
-  this.death = function() {
-    for (var i = 0; i < this.tail.length; i++) {
-      var pos = this.tail[i];
-      var d = dist(this.x, this.y, pos.x, pos.y);
-      if (d < 1) {
-        console.log('starting over');
-        this.total = 0;
-        this.tail = [];
-      }
-    }
-  }
+	this.show = function(){
+		fill(255);
+		for(var i = 0; i < this.tail.length; i++){
+			rect(this.tail[i].x, this.tail[i].y, gridScale, gridScale);
+		}
+		rect(this.x, this.y, gridScale, gridScale);
+	}
 
-  this.update = function() {
-    if (this.total === this.tail.length) {
-      for (var i = 0; i < this.tail.length - 1; i++) {
-        this.tail[i] = this.tail[i + 1];
-      }
-    }
-    this.tail[this.total - 1] = createVector(this.x, this.y);
+	this.dir = function(x, y){
+		this.xspeed = x;
+		this.yspeed = y;
+	}
 
-    this.x = this.x + this.xspeed * scl;
-    this.y = this.y + this.yspeed * scl;
+	this.ateFood = function(pos){
+		d = dist(this.x, this.y, pos.x, pos.y)
+		if(d < 1){
+			this.total++;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 
-    this.x = constrain(this.x, 0, width - scl);
-    this.y = constrain(this.y, 0, height - scl);
-  }
+	this.isDead = function(){
+		for(var i = 0; i < this.tail.length; i++){
+			var pos = this.tail[i];
+			var d = dist(this.x, this.y, pos.x, pos.y);
+			if(d < 1){
+				gameState = 'end';
+			}
+		}
 
-  this.show = function() {
-    fill(255);
-    for (var i = 0; i < this.tail.length; i++) {
-      rect(this.tail[i].x, this.tail[i].y, scl, scl);
-    }
-    rect(this.x, this.y, scl, scl);
+		var hit_wall = (this.x + gridScale > width || this.x + gridScale < 0) || ((this.y + gridScale > height || this.y + gridScale < 0));
+		// var hit_wall = (this.x > width || this.x < 0) || ((this.y > height || this.y < 0));
+		// console.log(this.x);
+		// console.log(width);
+		// console.log(hit_wall);
 
-  }
+		if(hit_wall){
+			gameState = 'end';
+		}
+	}
 }
