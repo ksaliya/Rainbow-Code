085751f6c8b452bf928b69c2bd1efd8ec00e3935
ksaliya <saliyakumara@yahoo.com>
2017-03-23 15:58:51 -0400

    Update sketch.js
    Added a gameState variable and a simple game menu

--- challenges/CC_03_Snake_game_p5.js/sketch.js
+++ challenges/CC_03_Snake_game_p5.js/sketch.js
@@ -1,59 +1,90 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/AaGK-fj-BAM
-
 var s;
-var scl = 20;
-
+var gridScale = 20;
 var food;
+var gameState;
+// var logo;
 
 function setup() {
-  createCanvas(600, 600);
-  s = new Snake();
-  frameRate(10);
-  pickLocation();
+	createCanvas(500, 500);
+	frameRate(10);
+	// logo = loadImage('logo.png');
+	gameState = 'init';
 
 }
 
-function pickLocation() {
-  var cols = floor(width/scl);
-  var rows = floor(height/scl);
-  food = createVector(floor(random(cols)), floor(random(rows)));
-  food.mult(scl);
+function pickLocation(){
+	var cols = floor(width/gridScale);
+	var rows = floor(height/gridScale);
+	food = createVector(floor(random(cols)), floor(random(rows)));
+	food.mult(gridScale);
 }
 
-function mousePressed() {
-  s.total++;
+function initGame(){
+	// image(logo, (width - logo.width)/2, (height - logo.height)/2);
+	var name = 'Snake Game';
+	nameWidht = textWidth(name);
+	textSize(50);
+	fill(255);
+	text(name, (width - nameWidht)/2, height/2 - 40);
+	startBtn = createButton('Start Game');
+	startBtn.position(width/2 - startBtn.width/2, height/2);
+	startBtn.mousePressed(startGame);
+}
+
+function startGame(){
+	removeElements();
+	gameState = 'play';
+	s = new Snake;
+	pickLocation();
+}
+
+function endGame(){
+	var msg = 'Game Over';
+		var score = 'Your Score is ' + (s.total - 1);
+		msgWidht = textWidth(msg);
+		scoreWidht = textWidth(score);
+		textSize(32);
+		fill(255);
+		text(msg, (width - msgWidht)/2, height/2 - 40);
+		text(score, (width - scoreWidht)/2, height/2);
+		startBtn = createButton('Restart Game');
+		startBtn.position(width/2 - startBtn.width/2, height/2 + 40);
+		startBtn.mousePressed(startGame);
 }
 
 function draw() {
-  background(51);
+	background(50);
+	if(gameState == 'init'){
+		initGame();
+	}
+	else if(gameState == 'play'){
+		s.isDead();
+		s.update();
+		s.show();
 
-  if (s.eat(food)) {
-    pickLocation();
-  }
-  s.death();
-  s.update();
-  s.show();
+		if(s.ateFood(food)){
+			pickLocation();
+		}
 
-
-  fill(255, 0, 100);
-  rect(food.x, food.y, scl, scl);
+		fill(255, 0 , 100);
+		rect(food.x, food.y, gridScale, gridScale);
+	}
+	else if(gameState == 'end'){
+		endGame();
+	}
 }
 
-
-
-
-
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
-  }
+function keyPressed(){
+	if(keyCode === UP_ARROW && s.yspeed != 1){
+		s.dir(0, -1);
+	}
+	else if(keyCode === DOWN_ARROW && s.yspeed != -1){
+		s.dir(0, 1);
+	}
+	else if(keyCode === LEFT_ARROW && s.xspeed != 1){
+		s.dir(-1, 0);
+	}
+	else if(keyCode === RIGHT_ARROW && s.xspeed != -1){
+		s.dir(1, 0);
+	}
 }
